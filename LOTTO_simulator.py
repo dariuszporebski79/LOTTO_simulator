db197917272ece44ddcae26339d5e81015c3d77a
dariuszporebski79 <dariusz.porebski79@wp.pl>
2023-04-01 22:40:58 +0200

    with the first function

--- LOTTO_simulator.py
+++ LOTTO_simulator.py
@@ -1,20 +1,33 @@
 from random import shuffle
-number1 = int(input("Podaj liczbę od 1 do 49: "))
-number2 = int(input("Podaj liczbę od 1 do 49: "))
-number3 = int(input("Podaj liczbę od 1 do 49: "))
-number4 = int(input("Podaj liczbę od 1 do 49: "))
-number5 = int(input("Podaj liczbę od 1 do 49: "))
-number6 = int(input("Podaj liczbę od 1 do 49: "))
-player_numbers = [number1, number2, number3, number4, number5, number6]
+
+
+def get_player_numbers():
+    player_numbers = []
+    for i in range(6):
+        while True:
+            try:
+                number = int(input("Podaj liczbę od 1 do 49: "))
+                if (1 <= number <= 49) and (number not in player_numbers):
+                    player_numbers.append(number)
+                    break
+                else:
+                    print("Wpisano nieprawidłowe dane. Podaj liczbę od 1 do 49."
+                          "\nLiczby nie mogą się powtarzać: ")
+            except ValueError:
+                print("Wpisano nieprawidłowe dane. Podaj liczbę od 1 do 49: ")
+    return player_numbers
+
+
+player_numbers = get_player_numbers()
 player_numbers.sort()
-print(player_numbers)
+print("Twoje liczby: ", player_numbers)
 LOTTO = [i+1 for i in range(0, 49)]
 shuffle(LOTTO)
 LOTTO_results = (LOTTO[:6])
 LOTTO_results.sort()
-print(LOTTO_results)
+print("Wylosowane liczby LOTTO", LOTTO_results)
 happy_numbers = 0
 for player_number in player_numbers:
     if player_number in LOTTO_results:
         happy_numbers += 1
-print(happy_numbers)
+print(f'Trafiłeś {happy_numbers} liczbę/y')
